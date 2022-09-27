741baf5b10f9a326712af2b17ee45a5398764a7b
SeanTheShe3p <swiddowson@students.mchenry.edu>
2022-09-26 22:17:48 -0500

    initial upload

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,16 @@
 fun main(args: Array<String>) {
     var employee1 = EmployeeInfo("Sean Widdowson",22135,1)
     var employee2 = EmployeeInfo("Jason", 22136,2)
-    var employee3 = EmployeeInfo("Freddie", 221337, 3)
+    var employee3 = EmployeeInfo("Freddie", 221337, 3) //Add employees
     employee1.printAll()
     employee2.printAll()
     employee3.printAll()
     employee2.name = "Jeepers"
-    employee3.shift = 1
+    employee3.shift = 1 // set employee name and shift
     employee1.printAll()
     employee2.printAll()
     employee3.printAll()
+    println(employee2.name)
+    println(employee3.shift)
+    println(employee1.employeeNumber)
 }
