acf5962e039318d67fb11abb3e4a9df127da7691
SeanTheShe3p <swiddowson@students.mchenry.edu>
2022-09-26 22:19:03 -0500

    Add comments and get / set

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,13 +4,13 @@ fun main(args: Array<String>) {
     var employee3 = EmployeeInfo("Freddie", 221337, 3) //Add employees
     employee1.printAll()
     employee2.printAll()
-    employee3.printAll()
+    employee3.printAll() // formatted data
     employee2.name = "Jeepers"
     employee3.shift = 1 // set employee name and shift
     employee1.printAll()
     employee2.printAll()
-    employee3.printAll()
+    employee3.printAll() // formatted new data
     println(employee2.name)
     println(employee3.shift)
-    println(employee1.employeeNumber)
+    println(employee1.employeeNumber) //get properties
 }
